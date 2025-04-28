8e89362383d09b512646116bc9abfaaf635ce07a
Devix1 <devixgit@gmail.com>
2025-04-28 21:35:54 +0600

    Create shaders vertex && fragment

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,60 @@
 
 using namespace std;
 
+// Вершины треугольника
+GLfloat points[] = {
+     0.0f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+// Цвета для каждой вершины
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f, // Красный
+    0.0f, 1.0f, 0.0f, // Зелёный
+    0.0f, 0.0f, 1.0f  // Синий
+};
+
+// Вершинный шейдер
+const char* vertexShaderSource = R"(
+#version 460 core
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexColor;
+out vec3 color;
+void main() {
+    color = vertexColor;
+    gl_Position = vec4(vertexPosition, 1.0);
+}
+)";
+
+// Фрагментный шейдер
+const char* fragmentShaderSource = R"(
+#version 460 core
+in vec3 color;
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(color, 1.0);
+}
+)";
+
 int WindowWidth = 640;
 int WindowHeight = 480;
 
-void windowSixeCallBack(GLFWwindow* pWindow, int width, int height){
+void windowSizeCallback(GLFWwindow* window, int width, int height) {
     WindowWidth = width;
-    WindowHeight = WindowHeight;
+    WindowHeight = height;
     glViewport(0, 0, WindowWidth, WindowHeight);
 }
 
-void glfwKeyCallBack(GLFWwindow* pWindow, int key, int scancode, int action, int mode){
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
-        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
     }
 }
 
-int main(void)
-{
-   
-
-   
-    if (!glfwInit()){
-        cout<<"glfwInit failed"<<endl;
+int main() {
+    if (!glfwInit()) {
+        cout << "glfwInit failed" << endl;
         return -1;
     }
 
@@ -33,19 +65,16 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    
-    GLFWwindow* pWindow = glfwCreateWindow(WindowWidth, WindowHeight, "Battle Tanks", nullptr, nullptr);
-    if (!pWindow)
-    {
-        cout<<"glfwCreateWindow failed"<<endl;
+    GLFWwindow* window = glfwCreateWindow(WindowWidth, WindowHeight, "Battle Tanks", nullptr, nullptr);
+    if (!window) {
+        cout << "glfwCreateWindow failed" << endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwSetWindowSizeCallback(pWindow, windowSixeCallBack);
-    glfwSetKeyCallback(pWindow, glfwKeyCallBack);
-   
-    glfwMakeContextCurrent(pWindow);
+    glfwSetWindowSizeCallback(window, windowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+    glfwMakeContextCurrent(window);
 
     if (!gladLoadGL()) {
         cout << "Can't load GLAD" << endl;
@@ -56,23 +85,63 @@ int main(void)
     glGetIntegerv(GL_MAJOR_VERSION, &major);
     glGetIntegerv(GL_MINOR_VERSION, &minor);
 
-    cout<<"Renderer"<<glGetString(GL_RENDERER) << endl;
-    cout<<"OpenGL version: "<< glGetString(GL_VERSION)<<endl;
-    cout << "OpenGL version: " << major << "." << minor << endl;
-    cout<< "Press Escape to exit" << endl;
+    cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
+    cout << "OpenGL version (parsed): " << major << "." << minor << endl;
+    cout << "Press Escape to exit" << endl;
 
-    glClearColor(1,1,0,1);
-    
-    
-    while (!glfwWindowShouldClose(pWindow))
-    {
-       
+    glClearColor(1, 1, 0, 1); // Жёлтый фон
+
+    // Компиляция шейдеров
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Настройка VAO
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Буфер вершин
+    GLuint pointsVbo;
+    glGenBuffers(1, &pointsVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, pointsVbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    // Буфер цветов
+    GLuint colorsVbo;
+    glGenBuffers(1, &colorsVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colorsVbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    // Отвязать VAO
+    glBindVertexArray(0);
+
+    // Главный цикл
+    while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-       
-        glfwSwapBuffers(pWindow);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-       
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
